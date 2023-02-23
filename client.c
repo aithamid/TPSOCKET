967cdf0f9cb968dbd354243a39f7f30d777e1bac
aithamid <aithamidbusiness@gmail.com>
2023-02-23 15:24:00 +0100

    quit option

--- client.c
+++ client.c
@@ -1,35 +1,56 @@
-#include <netinet/in.h> //structure for storing address information
+// Client side C/C++ program to demonstrate Socket
+// programming
+#include <arpa/inet.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h> //for socket APIs
-#include <sys/types.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <time.h>
+#define PORT 8080
 
 int main(int argc, char const* argv[])
 {
-	int sockD = socket(AF_INET, SOCK_STREAM, 0);
-
-	struct sockaddr_in servAddr;
-
-	servAddr.sin_family = AF_INET;
-	servAddr.sin_port
-		= htons(9001); // use some unused port number
-	servAddr.sin_addr.s_addr = INADDR_ANY;
-
-	int connectStatus
-		= connect(sockD, (struct sockaddr*)&servAddr,
-				sizeof(servAddr));
-
-	if (connectStatus == -1) {
-		printf("Error...\n");
+	int status, valread, client_fd;
+	struct sockaddr_in serv_addr;
+	char* hello = "BLABLA";
+	char buffer[1024] = { 0 };
+	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
 	}
 
-	else {
-		char strData[255];
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
 
-		recv(sockD, strData, sizeof(strData), 0);
-
-		printf("Message: %s\n", strData);
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+		<= 0) {
+		printf(
+			"\nInvalid address/ Address not supported \n");
+		return -1;
 	}
 
+	if ((status
+		= connect(client_fd, (struct sockaddr*)&serv_addr,
+				sizeof(serv_addr)))
+		< 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	while(1)
+	{	
+		char message[20];
+		printf("Sent : \t\t");
+		scanf("%s", message);
+		send(client_fd, message, strlen(message), 0);
+		valread = read(client_fd, buffer, 1024);
+		if(buffer[0] != '\0')    
+        	printf("Received : \t%s\n", buffer);
+
+		memset(buffer, 0, 1024);
+	}
+	// closing the connected socket
+	close(client_fd);
 	return 0;
 }
--- server.c
+++ server.c
@@ -6,21 +6,30 @@
 
 int main(int argc, char const* argv[])
 {
+    int port=9001;
+    
+    printf("%d", argc);
+    if(argc>1)
+    {
+        printf("CHANGE");
+        port = strtol(argv[1], NULL, 10);
+    } 
+
+
 
 	// create server socket similar to what was done in
 	// client program
 	int servSockD = socket(AF_INET, SOCK_STREAM, 0);
 
 	// string store data to send to client
-	char serMsg[255] = "Message from the server to the "
-					"client \'Hello Client\' ";
+	char serMsg[255] = "Message du serveur c'est Adam";
 
 	// define server address
 	struct sockaddr_in servAddr;
 
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_port = htons(9001);
-	servAddr.sin_addr.s_addr = INADDR_ANY;
+	servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	// bind socket to the specified IP and port
 	bind(servSockD, (struct sockaddr*)&servAddr,
